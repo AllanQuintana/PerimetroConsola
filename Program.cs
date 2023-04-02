8bf618bd1df022253560661839a64f42a78d15ae
ALLAN SEBASTIAN QUINTANA ABARCA <allan.quintana@gmail.com>
2023-04-02 16:55:05 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace ConPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarativa de variables
+            double alto, ancho, perimetro;
+            string strAlto, strAncho;
+            Console.WriteLine("Ingrese el alto del rectangulo ");
+            strAlto = Console.ReadLine();
+            Console.WriteLine("Ingrese el ancho del rectangulo ");
+            strAncho = Console.ReadLine();
+
+            // Convertir las variables string a variables numericas
+            double.TryParse(strAlto, out alto);
+            double.TryParse(strAncho, out ancho);
+            perimetro = 2 * (alto + ancho);
+            Console.WriteLine("El perimetro es " + perimetro);
+            Console.ReadKey();
+        }
+    }
+}
